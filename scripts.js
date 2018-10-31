a42a6898a456dbe85d2bb9ce65977a2cad71f416
Olga <olga.myazina@outlook.com>
2018-10-31 10:28:28 +0300

    Добавила стрелочных ф-ий

--- scripts.js
+++ scripts.js
@@ -10,12 +10,12 @@ const devices = document.querySelector('.devices');
 const pagiantorDevs = document.querySelector('.devices__paginator');
 let currentPageDevs = 1;
 pagiantorDevs.classList.toggle('paginator_hide', panelCountDevs < 7);
-arrowRightDevs.addEventListener('click', function() {
+arrowRightDevs.addEventListener('click', () => {
   currentPageDevs += 1;
   arrowLeftDevs.classList.toggle('paginator__arrow_disabled', currentPageDevs === 1);
   devices.scroll({ top: 0, left: 1366, behavior: 'smooth' });
 });
-arrowLeftDevs.addEventListener('click', function() {
+arrowLeftDevs.addEventListener('click', () => {
   if (currentPageDevs > 1) {
     currentPageDevs -= 1;
     arrowLeftDevs.classList.toggle('paginator__arrow_disabled', currentPageDevs === 1);
@@ -129,7 +129,7 @@ const scenarios = document.querySelector('.scenarios');
 const pagiantorScens = document.querySelector('.scenarios__paginator');
 let currentPage = 1;
 pagiantorScens.classList.toggle('paginator_hide', panelCountScens <= 9);
-arrowRightScens.addEventListener('click', function() {
+arrowRightScens.addEventListener('click', () => {
   if (currentPage < pageCountScens) {
     currentPage += 1;
     arrowRightScens.classList.toggle('paginator__arrow_disabled', currentPage === pageCountScens);
@@ -137,7 +137,7 @@ arrowRightScens.addEventListener('click', function() {
     scenarios.scroll({ top: 0, left: 645, behavior: 'smooth' });
   }
 });
-arrowLeftScens.addEventListener('click', function() {
+arrowLeftScens.addEventListener('click', () => {
   if (currentPage > 1) {
     currentPage -= 1;
     arrowRightScens.classList.toggle('paginator__arrow_disabled', currentPage === pageCountScens);
@@ -149,7 +149,7 @@ const selectButton = document.querySelector('.filter__select-button');
 const selectButtonText = document.querySelector('.filter__select-button .button__text');
 const selectOptions = document.querySelectorAll('.filter__select-item');
 const popup = document.querySelector('.filter__select-popup');
-selectButton.addEventListener('click', function() {
+selectButton.addEventListener('click', () => {
   popup.classList.toggle('filter__select-popup_open');
 }); /*
 let widths = '';
