eba065421e04dc3a8c858596e3d593efa7e1b92e
Olga <olga.myazina@outlook.com>
2018-10-31 10:45:59 +0300

    Вроде работает js, без минификации

--- scripts.js
+++ scripts.js
@@ -144,28 +144,7 @@ arrowLeftScens.addEventListener('click', () => {
     arrowLeftScens.classList.toggle('paginator__arrow_disabled', currentPage === 1);
     scenarios.scroll({ top: 0, left: -645, behavior: 'smooth' });
   }
-});
-const selectButton = document.querySelector('.filter__select-button');
-const selectButtonText = document.querySelector('.filter__select-button .button__text');
-const selectOptions = document.querySelectorAll('.filter__select-item');
-const popup = document.querySelector('.filter__select-popup');
-selectButton.addEventListener('click', () => {
-  popup.classList.toggle('filter__select-popup_open');
-}); /*
-let widths = '';
-window.addEventListener('scroll', function() {
-  widths += document.querySelectorAll('body')[0].offsetWidth;
-  document.querySelector('.stats').innerHTML = widths;
-});*/
-selectOptions.forEach(o => {
-  o.addEventListener('click', e => {
-    document.querySelector('#' + e.target.dataset.group).checked = true;
-    selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
-    e.target.classList.toggle('filter__select-item_checked', true);
-    popup.classList.toggle('filter__select-popup_open', false);
-    selectButtonText.innerText = e.target.innerText;
-  });
-});
+}); //const selectButton = document.querySelector('.filter__select-button');//const selectButtonText = document.querySelector('.filter__select-button .button__text');//const selectOptions = document.querySelectorAll('.filter__select-item');//const popup = document.querySelector('.filter__select-popup');/*selectButton.addEventListener('click', function() {popup.classList.toggle('filter__select-popup_open');});/*let widths = '';window.addEventListener('scroll', function() {widths += document.querySelectorAll('body')[0].offsetWidth;document.querySelector('.stats').innerHTML = widths;});*//*selectOptions.forEach(o => {o.addEventListener('click', e => {document.querySelector('#' + e.target.dataset.group).checked = true;selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));e.target.classList.toggle('filter__select-item_checked', true);popup.classList.toggle('filter__select-popup_open', false);selectButtonText.innerText = e.target.innerText;});});*/
 document.querySelector('.menu__icon').addEventListener('click', () => {
   document.querySelector('.menu').classList.toggle('menu_open');
 });
